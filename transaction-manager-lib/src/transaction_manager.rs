a04528782d95d300864e8d2f1ab8a561cb3f18d6
Pete LeVasseur <plevasseur@gmail.com>
2024-09-28 11:13:01 -0400

    Add skeleton for how to handle the different transactions

--- transaction-manager-lib/src/transaction_manager.rs
+++ transaction-manager-lib/src/transaction_manager.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use log::*;
-use crate::transactions::Transaction;
+use std::sync::Arc;
+use crate::transactions::{Transaction, Withdrawal, Deposit, Chargeback, Resolve, Dispute};
 use crate::balance::ClientBalanceRegistry;
 
 #[derive(Debug)]
@@ -19,20 +20,51 @@ impl fmt::Display for TransactionManagerError {
 
 impl std::error::Error for TransactionManagerError {}
 
-pub struct TransactionManager {}
+pub struct TransactionManager {
+    balances: Arc<ClientBalanceRegistry>
+}
 
 impl TransactionManager {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            balances: Arc::new(ClientBalanceRegistry::new())
+        }
     }
 
     pub fn attempt_insertion(&mut self, t: &Transaction) -> Result<(), TransactionManagerError> {
         debug!("Transaction: {t:?}");
 
+        match t {
+            Transaction::Withdrawal(w) => self.handle_withdrawal(w),
+            Transaction::Deposit(d) => self.handle_deposit(d),
+            Transaction::Chargeback(c) => self.handle_chargeback(c),
+            Transaction::Resolve(r) => self.handle_resolve(r),
+            Transaction::Dispute(d) => self.handle_dispute(d),
+        }
+    }
+
+    pub fn retrieve_client_balances(&self) -> Arc<ClientBalanceRegistry> {
+        self.balances.clone()
+    }
+
+    fn handle_withdrawal(&mut self, w: &Withdrawal) -> Result<(), TransactionManagerError> {
         Ok(())
     }
 
-    pub fn retrieve_client_balances(&self) -> ClientBalanceRegistry {
-        ClientBalanceRegistry::new()
+    fn handle_deposit(&mut self, d: &Deposit) -> Result<(), TransactionManagerError> {
+        Ok(())
+    }
+
+    fn handle_chargeback(&mut self, c: &Chargeback) -> Result<(), TransactionManagerError> {
+        Ok(())
+    }
+
+    fn handle_dispute(&mut self, d: &Dispute) -> Result<(), TransactionManagerError> {
+        Ok(())
+    }
+
+    fn handle_resolve(&mut self, r: &Resolve) -> Result<(), TransactionManagerError> {
+        Ok(())
     }
 }
+
