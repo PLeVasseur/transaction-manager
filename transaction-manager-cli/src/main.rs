df580b1371d5cd304ac4e6087328d0b56df1f351
Pete LeVasseur <plevasseur@gmail.com>
2024-09-28 09:40:22 -0400

    Read CSV as string only, ignore malformed transactions

--- transaction-manager-cli/src/main.rs
+++ transaction-manager-cli/src/main.rs
@@ -1,11 +1,29 @@
 use clap::Parser;
+use std::fs::File;
+use std::error::Error;
+use csv::Reader;
 
 mod cli;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = cli::Cli::parse();
 
     println!("cli: {:?}", cli.input);
 
+    let file = File::open(cli.input)?;
+    let mut rdr = Reader::from_reader(file);
+
+    for result in rdr.records() {
+        let Ok(transaction) = result else {
+            // If we get a malformed transaction, we just
+            // continue to read
+            continue;
+        };
+
+        println!("transaction: {transaction:?}");
+    }
+
     println!("Hello, world!");
+
+    Ok(())
 }
