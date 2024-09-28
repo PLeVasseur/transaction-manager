21ea0a4eb32319c7f98d42aeaafcac467175e821
Pete LeVasseur <plevasseur@gmail.com>
2024-09-28 10:19:49 -0400

    Able to deserialize transactions.csv into properly formatting Transaction

--- transaction-manager-cli/src/main.rs
+++ transaction-manager-cli/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use std::fs::File;
 use std::error::Error;
-use csv::Reader;
+use csv::ReaderBuilder;
+use transaction_manager_lib::transactions::{Transaction};
 
 mod cli;
 
@@ -11,9 +12,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("cli: {:?}", cli.input);
 
     let file = File::open(cli.input)?;
-    let mut rdr = Reader::from_reader(file);
 
-    for result in rdr.records() {
+    // Configure the CSV reader to trim headers and fields of whitespace
+    let mut rdr = ReaderBuilder::new()
+        .trim(csv::Trim::All) // Trims all fields including headers
+        .from_reader(file);
+
+    for result in rdr.deserialize::<Transaction>() {
+        println!("result: {result:?}");
+
         let Ok(transaction) = result else {
             // If we get a malformed transaction, we just
             // continue to read
---
+++ transaction-manager-lib/src/transactions.rs
@@ -0,0 +1,100 @@
+use serde::de::{self, Deserializer};
+use serde::{Deserialize};
+
+#[derive(Debug)]
+pub struct Deposit {
+    pub client: u16,
+    pub tx: u32,
+    pub amount: f64,
+}
+
+#[derive(Debug)]
+pub struct Withdrawal {
+    pub client: u16,
+    pub tx: u32,
+    pub amount: f64,
+}
+
+#[derive(Debug)]
+pub struct Dispute {
+    pub client: u16,
+    pub tx: u32,
+}
+
+#[derive(Debug)]
+pub struct Resolve {
+    pub client: u16,
+    pub tx: u32,
+}
+
+#[derive(Debug)]
+pub struct Chargeback {
+    pub client: u16,
+    pub tx: u32,
+}
+
+#[derive(Debug)]
+pub enum Transaction {
+    Deposit(Deposit),
+    Withdrawal(Withdrawal),
+    Dispute(Dispute),
+    Resolve(Resolve),
+    Chargeback(Chargeback),
+}
+
+impl<'de> Deserialize<'de> for Transaction {
+    fn deserialize<D>(deserializer: D) -> Result<Transaction, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct TransactionRecord {
+            #[serde(rename = "type")]
+            tx_type: String,
+            client: u16,
+            tx: u32,
+            // Optional, since not all types use the transaction amount
+            amount: Option<f64>, 
+        }
+
+        let record = TransactionRecord::deserialize(deserializer)?;
+
+        match record.tx_type.as_str() {
+            "deposit" => {
+                if let Some(amount) = record.amount {
+                    Ok(Transaction::Deposit(Deposit {
+                        client: record.client,
+                        tx: record.tx,
+                        amount,
+                    }))
+                } else {
+                    Err(de::Error::custom("Missing amount for deposit"))
+                }
+            }
+            "withdrawal" => {
+                if let Some(amount) = record.amount {
+                    Ok(Transaction::Withdrawal(Withdrawal {
+                        client: record.client,
+                        tx: record.tx,
+                        amount,
+                    }))
+                } else {
+                    Err(de::Error::custom("Missing amount for withdrawal"))
+                }
+            }
+            "dispute" => Ok(Transaction::Dispute(Dispute {
+                client: record.client,
+                tx: record.tx,
+            })),
+            "resolve" => Ok(Transaction::Resolve(Resolve {
+                client: record.client,
+                tx: record.tx,
+            })),
+            "chargeback" => Ok(Transaction::Chargeback(Chargeback {
+                client: record.client,
+                tx: record.tx,
+            })),
+            _ => Err(de::Error::custom("Unknown transaction type")),
+        }
+    }
+}
